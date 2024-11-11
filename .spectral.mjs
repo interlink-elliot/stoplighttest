d64433d3071c34ed8dbb1f6eb2f691880e823a27
Elliot Pagent-Froggatt <elliot.pagent-froggatt@interlinksoftware.com>
2024-11-11 13:54:11 +0000

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI2MzkyOA/spectral.js?branch_slug=main&token=6b75c599-0fcf-43c9-80b1-f8f6b896f39e";
+export default { extends: ruleset };
